1aee944490f82e1465cd939682d2ab15199950b5
mustafa8907-png <mustafatm214i@gmail.com>
2026-01-11 13:04:03 +0300

    Create HTHomes.java

---
+++ src/main/java/com/hthomes/HTHomes.java
@@ -0,0 +1,29 @@
+package com.hthomes;
+
+import com.hthomes.commands.HomeCommand;
+import com.hthomes.listeners.GUIListener;
+import com.hthomes.managers.HomeManager;
+import com.hthomes.managers.LanguageManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HTHomes extends JavaPlugin {
+    private static HTHomes instance;
+    private HomeManager homeManager;
+    private LanguageManager languageManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        this.languageManager = new LanguageManager(this);
+        this.homeManager = new HomeManager(this);
+        getCommand("home").setExecutor(new HomeCommand(this));
+        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+    }
+
+    @Override
+    public void onDisable() { if (homeManager != null) homeManager.saveHomes(); }
+    public static HTHomes getInstance() { return instance; }
+    public HomeManager getHomeManager() { return homeManager; }
+    public LanguageManager getLanguageManager() { return languageManager; }
+  }
