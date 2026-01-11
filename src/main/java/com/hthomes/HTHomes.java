b4300e3de184921df063efb4ae7d81ca1415696b
mustafa8907-png <mustafatm214i@gmail.com>
2026-01-11 21:15:02 +0300

    Update HTHomes.java

--- src/main/java/com/hthomes/HTHomes.java
+++ src/main/java/com/hthomes/HTHomes.java
@@ -15,15 +15,31 @@ public class HTHomes extends JavaPlugin {
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
+        
+        // Managerları yükle
         this.languageManager = new LanguageManager(this);
         this.homeManager = new HomeManager(this);
+        
+        // Komut ve Eventleri kaydet
         getCommand("home").setExecutor(new HomeCommand(this));
+        getCommand("sethome").setExecutor(new HomeCommand(this));
+        getCommand("delhome").setExecutor(new HomeCommand(this));
+        
         getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+        
+        // Havalı Açılış Mesajı
+        getLogger().info("\n§a-----------------------------\n§6HT-Homes+ §eAktif Edildi!\n§bVersiyon: 2.1.0\n§fYazar: Mustafa8907\n§a-----------------------------");
     }
 
     @Override
-    public void onDisable() { if (homeManager != null) homeManager.saveHomes(); }
+    public void onDisable() {
+        if (homeManager != null) {
+            homeManager.saveHomes();
+        }
+        getLogger().info("\n§c-----------------------------\n§4HT-Homes+ §cDevre Dışı!\n§cVeriler Kaydedildi.\n§c-----------------------------");
+    }
+
     public static HTHomes getInstance() { return instance; }
     public HomeManager getHomeManager() { return homeManager; }
     public LanguageManager getLanguageManager() { return languageManager; }
-  }
+                                                }
