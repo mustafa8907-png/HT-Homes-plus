abadac53fafa4a746b3d0cf7ae81529956e6318a
mustafa8907-png <mustafatm214i@gmail.com>
2026-01-11 21:50:49 +0300

    Update HTHomes.java

--- src/main/java/com/hthomes/HTHomes.java
+++ src/main/java/com/hthomes/HTHomes.java
@@ -1,5 +1,7 @@
 package com.hthomes;
 
+package com.hthomes;
+
 import com.hthomes.commands.HomeCommand;
 import com.hthomes.listeners.GUIListener;
 import com.hthomes.managers.HomeManager;
@@ -14,32 +16,26 @@ public class HTHomes extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        saveDefaultConfig();
+        saveDefaultConfig(); // Configi kaydet
         
-        // Managerları yükle
         this.languageManager = new LanguageManager(this);
         this.homeManager = new HomeManager(this);
         
-        // Komut ve Eventleri kaydet
         getCommand("home").setExecutor(new HomeCommand(this));
         getCommand("sethome").setExecutor(new HomeCommand(this));
         getCommand("delhome").setExecutor(new HomeCommand(this));
         
         getServer().getPluginManager().registerEvents(new GUIListener(this), this);
         
-        // Havalı Açılış Mesajı
-        getLogger().info("\n§a-----------------------------\n§6HT-Homes+ §eAktif Edildi!\n§bVersiyon: 2.1.0\n§fYazar: Mustafa8907\n§a-----------------------------");
+        getLogger().info("HT-Homes+ v2.1.0 (GUI Fix + Page System) Aktif!");
     }
 
     @Override
     public void onDisable() {
-        if (homeManager != null) {
-            homeManager.saveHomes();
-        }
-        getLogger().info("\n§c-----------------------------\n§4HT-Homes+ §cDevre Dışı!\n§cVeriler Kaydedildi.\n§c-----------------------------");
+        if (homeManager != null) homeManager.saveHomes();
     }
 
     public static HTHomes getInstance() { return instance; }
     public HomeManager getHomeManager() { return homeManager; }
     public LanguageManager getLanguageManager() { return languageManager; }
-                                                }
+                                                                      }
