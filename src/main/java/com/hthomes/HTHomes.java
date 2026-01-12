d312413059e6c4d01b5225588e4c7feafe2c7814
mustafa8907-png <mustafatm214i@gmail.com>
2026-01-12 18:02:19 +0300

    Update HTHomes.java

--- src/main/java/com/hthomes/HTHomes.java
+++ src/main/java/com/hthomes/HTHomes.java
@@ -5,6 +5,7 @@ import com.hthomes.listeners.GUIListener;
 import com.hthomes.managers.HomeManager;
 import com.hthomes.managers.LanguageManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
 
 public class HTHomes extends JavaPlugin {
     private static HTHomes instance;
@@ -14,17 +15,39 @@ public class HTHomes extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        
+        // 1. Önce config.yml'yi oluştur
         saveDefaultConfig();
         
+        // 2. Dil dosyalarını klasöre dök (Eksik olanları çıkartır)
+        exportDefaultLanguages();
+        
+        // 3. Manager'ları başlat
         this.languageManager = new LanguageManager(this);
         this.homeManager = new HomeManager(this);
         
+        // 4. Komutlar ve Listenerlar
         getCommand("home").setExecutor(new HomeCommand(this));
         getCommand("sethome").setExecutor(new HomeCommand(this));
         getServer().getPluginManager().registerEvents(new GUIListener(this), this);
     }
 
+    private void exportDefaultLanguages() {
+        String[] languages = {"en", "tr", "ru", "ar", "es", "hi", "de", "pt", "ja", "br", "fr"};
+        File langDir = new File(getDataFolder(), "languages");
+        
+        if (!langDir.exists()) langDir.mkdirs();
+        
+        for (String lang : languages) {
+            String path = "languages/" + lang + ".yml";
+            File file = new File(getDataFolder(), path);
+            if (!file.exists()) {
+                saveResource(path, false);
+            }
+        }
+    }
+
     public static HTHomes getInstance() { return instance; }
     public HomeManager getHomeManager() { return homeManager; }
     public LanguageManager getLanguageManager() { return languageManager; }
-            }
+    }
