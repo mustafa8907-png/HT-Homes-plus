12af237720b56b63eb64c61bfda6dac9e23bd22e
mustafa8907-png <mustafatm214i@gmail.com>
2026-01-17 15:06:19 +0300

    Update HomeManager.java

--- src/main/java/com/hthomes/managers/HomeManager.java
+++ src/main/java/com/hthomes/managers/HomeManager.java
@@ -1,18 +1,16 @@
 package com.hthomes.managers;
 
 import com.hthomes.HTHomes;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeManager {
     private final HTHomes plugin;
@@ -21,10 +19,10 @@ public class HomeManager {
 
     public HomeManager(HTHomes plugin) {
         this.plugin = plugin;
-        setupHomes();
+        setup();
     }
 
-    private void setupHomes() {
+    public void setup() {
         homesFile = new File(plugin.getDataFolder(), "homes.yml");
         if (!homesFile.exists()) {
             try { homesFile.createNewFile(); } catch (IOException e) { e.printStackTrace(); }
@@ -32,69 +30,51 @@ public class HomeManager {
         homesConfig = YamlConfiguration.loadConfiguration(homesFile);
     }
 
-    public void setHome(Player player, String homeName) {
-        Location loc = player.getLocation();
-        String path = player.getUniqueId() + "." + homeName;
+    public void setHome(Player p, String name, Location loc) {
+        String path = p.getUniqueId() + "." + name;
         homesConfig.set(path + ".world", loc.getWorld().getName());
         homesConfig.set(path + ".x", loc.getX());
         homesConfig.set(path + ".y", loc.getY());
         homesConfig.set(path + ".z", loc.getZ());
         homesConfig.set(path + ".yaw", loc.getYaw());
         homesConfig.set(path + ".pitch", loc.getPitch());
-        homesConfig.set(path + ".icon", Material.LIME_BED.name()); // Varsayılan ikon
-        saveHomes();
+        if (!homesConfig.contains(path + ".icon")) homesConfig.set(path + ".icon", "LIME_BED");
+        save();
     }
 
-    public void deleteHome(Player player, String homeName) {
-        homesConfig.set(player.getUniqueId() + "." + homeName, null);
-        saveHomes();
+    public void setHomeIcon(Player p, String name, Material mat) {
+        homesConfig.set(p.getUniqueId() + "." + name + ".icon", mat.name());
+        save();
     }
 
-    public void teleportToHome(Player player, String homeName) {
-        String path = player.getUniqueId() + "." + homeName;
-        if (!homesConfig.contains(path)) return;
+    public Material getHomeIcon(Player p, String name) {
+        String matName = homesConfig.getString(p.getUniqueId() + "." + name + ".icon", "LIME_BED");
+        return Material.matchMaterial(matName);
+    }
 
-        Location loc = new Location(
-                plugin.getServer().getWorld(homesConfig.getString(path + ".world")),
-                homesConfig.getDouble(path + ".x"),
-                homesConfig.getDouble(path + ".y"),
-                homesConfig.getDouble(path + ".z"),
-                (float) homesConfig.getDouble(path + ".yaw"),
-                (float) homesConfig.getDouble(path + ".pitch")
-        );
-
-        player.closeInventory();
-        player.sendMessage(plugin.getLanguageManager().getMessage("messages.teleport_starting"));
-
-        new BukkitRunnable() {
-            int timeLeft = 3;
-            @Override
-            public void run() {
-                if (!player.isOnline()) { this.cancel(); return; }
-                if (timeLeft <= 0) {
-                    player.teleport(loc);
-                    player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 1f, 1f);
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§a§lHT-HOMES+"));
-                    this.cancel();
-                } else {
-                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent("§eTeleporting in: §6" + timeLeft));
-                    player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 2f);
-                    timeLeft--;
-                }
+    public Map<String, Location> getHomes(Player p) {
+        Map<String, Location> homes = new HashMap<>();
+        if (homesConfig.contains(p.getUniqueId().toString())) {
+            for (String key : homesConfig.getConfigurationSection(p.getUniqueId().toString()).getKeys(false)) {
+                String path = p.getUniqueId() + "." + key;
+                Location loc = new Location(
+                        plugin.getServer().getWorld(homesConfig.getString(path + ".world")),
+                        homesConfig.getDouble(path + ".x"),
+                        homesConfig.getDouble(path + ".y"),
+                        homesConfig.getDouble(path + ".z"),
+                        (float) homesConfig.getDouble(path + ".yaw"),
+                        (float) homesConfig.getDouble(path + ".pitch")
+                );
+                homes.put(key, loc);
             }
-        }.runTaskTimer(plugin, 0L, 20L);
+        }
+        return homes;
     }
 
-    public void setHomeIcon(Player player, String homeName, Material material) {
-        homesConfig.set(player.getUniqueId() + "." + homeName + ".icon", material.name());
-        saveHomes();
+    public void deleteHome(Player p, String name) {
+        homesConfig.set(p.getUniqueId() + "." + name, null);
+        save();
     }
 
-    public Material getHomeIcon(Player player, String homeName) {
-        String name = homesConfig.getString(player.getUniqueId() + "." + homeName + ".icon");
-        return name != null ? Material.valueOf(name) : Material.LIME_BED;
-    }
-
-    public FileConfiguration getHomesConfig() { return homesConfig; }
-    public void saveHomes() { try { homesConfig.save(homesFile); } catch (IOException e) { e.printStackTrace(); } }
-    }
+    public void save() { try { homesConfig.save(homesFile); } catch (IOException e) { e.printStackTrace(); } }
+}
