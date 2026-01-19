7ab34bc01f2cb8ac2a1d83596bf6850789d458cb
mustafa8907-png <mustafatm214i@gmail.com>
2026-01-19 14:46:26 +0300

    Update HTHomes.java

--- src/main/java/com/hthomes/HTHomes.java
+++ src/main/java/com/hthomes/HTHomes.java
@@ -1,40 +1,61 @@
-package com.hthomes;
+package com.hthomes.managers;
 
-import com.hthomes.commands.HomeCommand;
-import com.hthomes.listeners.GUIListener;
-import com.hthomes.managers.*;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.hthomes.HTHomes;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
-public class HTHomes extends JavaPlugin {
-    private static HTHomes instance;
-    private BukkitAudiences adventure;
-    private LanguageManager languageManager;
-    private HomeManager homeManager;
+public class HomeManager {
+    private final HTHomes plugin;
+    private FileConfiguration config;
+    private final File file;
 
-    @Override
-    public void onEnable() {
-        instance = this;
-        saveDefaultConfig();
-        this.adventure = BukkitAudiences.create(this);
-        this.homeManager = new HomeManager(this);
-        this.languageManager = new LanguageManager(this);
-        GUIManager.setPlugin(this);
-
-        HomeCommand cmd = new HomeCommand(this);
-        getCommand("home").setExecutor(cmd);
-        getCommand("homes").setExecutor(cmd);
-        getCommand("sethome").setExecutor(cmd);
-        getCommand("delhome").setExecutor(cmd);
-        
-        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+    public HomeManager(HTHomes plugin) {
+        this.plugin = plugin;
+        this.file = new File(plugin.getDataFolder(), "homes.yml");
+        this.config = YamlConfiguration.loadConfiguration(file);
     }
 
-    @Override
-    public void onDisable() { if(this.adventure != null) this.adventure.close(); }
-    public static HTHomes getInstance() { return instance; }
-    public BukkitAudiences getAdventure() { return adventure; }
-    public LanguageManager getLangManager() { return languageManager; }
-    public HomeManager getHomeManager() { return homeManager
-        ; }
+    public boolean exists(UUID uuid, String name) {
+        return config.contains(uuid.toString() + "." + name);
+    }
+
+    public void setHome(UUID uuid, String name, Location loc) {
+        String path = uuid.toString() + "." + name;
+        config.set(path + ".world", loc.getWorld().getName());
+        config.set(path + ".x", loc.getX());
+        config.set(path + ".y", loc.getY());
+        config.set(path + ".z", loc.getZ());
+        config.set(path + ".yaw", loc.getYaw());
+        config.set(path + ".pitch", loc.getPitch());
+        save();
+    }
+
+    public Location getHome(UUID uuid, String name) {
+        String path = uuid.toString() + "." + name;
+        if (!config.contains(path)) return null;
+        return new Location(Bukkit.getWorld(config.getString(path + ".world")), 
+            config.getDouble(path + ".x"), config.getDouble(path + ".y"), config.getDouble(path + ".z"), 
+            (float) config.getDouble(path + ".yaw"), (float) config.getDouble(path + ".pitch"));
+    }
+
+    public void deleteHome(UUID uuid, String name) {
+        config.set(uuid.toString() + "." + name, null);
+        save();
+    }
+
+    public List<String> getHomesList(UUID uuid) {
+        if (config.getConfigurationSection(uuid.toString()) == null) return new ArrayList<>();
+        return new ArrayList<>(config.getConfigurationSection(uuid.toString()).getKeys(false));
+    }
+
+    public void save() { try { config.save(file); } catch (IOException ignore
+                            d) {} }
     }
